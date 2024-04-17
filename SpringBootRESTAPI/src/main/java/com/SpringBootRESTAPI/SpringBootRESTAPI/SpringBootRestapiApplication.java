3b546a93caaade51e5172ed61b83707ecbb06ed8
Sure-sh554 <sureshrathod0113@gmail.com>
2024-04-17 15:04:10 +0530

    FirstRun

---
+++ SpringBootRESTAPI/src/main/java/com/SpringBootRESTAPI/SpringBootRESTAPI/SpringBootRestapiApplication.java
@@ -0,0 +1,14 @@
+package com.SpringBootRESTAPI.SpringBootRESTAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestapiApplication.class, args);
+		System.out.println("hello");
+	}
+
+}
